d0a0f36c662d395d9fe612804104cec01023f3eb
Kirill1708 <himangyclub@gmail.com>
2022-06-09 13:12:28 +0600

    views

--- exam/modelmanagement/views.py
+++ exam/modelmanagement/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
-# Create your views here.
+from .models import Restaurant, Dish
+from .serializers import BookSerializer, JournalSerializer
+
+
+class RestaurantListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        restaurants = Restaurant.objects.all()
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return Response(serializer.data)
+
+
+class BooksViewSet(ViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = [IsAdminUser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        instance = Restaurant.objects.get(id=pk)
+        serializer = BookSerializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, pk, *args, **kwargs):
+        instance = Restaurant.objects.get(id=pk)
+        serializer = self.serializer_class(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk, *args, **kwargs):
+        instance = Restaurant.objects.get(id=pk)
+        try:
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class DishListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        books = Dish.objects.all()
+        serializer = DishSerializer(books, many=True)
+        return Response(serializer.data)
+
+
+class DishViewSet(ViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+    permission_classes = [IsAdminUser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        instance = Dish.objects.get(id=pk)
+        serializer = DishSerializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, pk, *args, **kwargs):
+        instance = Dish.objects.get(id=pk)
+        serializer = self.serializer_class(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk, *args, **kwargs):
+        instance = Dish.objects.get(id=pk)
+        try:
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
